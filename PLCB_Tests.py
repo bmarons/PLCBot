835f7f1430bc3de8536877b3ffbbe7b9f6f6dae8
Barry <bma30@pitt.edu>
2015-04-10 18:55:17 -0400

    Test for get url

---
+++ PLCB_Tests.py
@@ -0,0 +1,8 @@
+import unittest
+class TestUM(unittest.TestCase):
+	def test_get_url(self):
+		'''This Test sends a URL to a function and should get back a soup object'''
+		self.fail("This has not been written yet")
+
+if __name__ == '__main__':
+    unittest.main()
