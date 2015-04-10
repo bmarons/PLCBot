ce0db7bd1beca2f0cece210902e249814b9b44cf
Barry <bma30@pitt.edu>
2015-04-10 19:10:24 -0400

    Made the Test Pass

--- PLCB_Tests.py
+++ PLCB_Tests.py
@@ -1,8 +1,11 @@
 import unittest
+import PLCBot
+
 class TestUM(unittest.TestCase):
 	def test_get_url(self):
-		'''This Test sends a URL to a function and should get back a soup object'''
-		self.fail("This has not been written yet")
+		"""This Test sends a URL to a function and should get back a soup object"""
+		testSoup = PLCBot.get_url("http://example.com/")
+		self.assertEqual(str(testSoup.html.head.title),"<title>Example Domain</title>")
 
 if __name__ == '__main__':
     unittest.main()
--- PLCBot.py
+++ PLCBot.py
@@ -2,3 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+
+def get_url(url):
+	"""Returns Soup Object of the given URL"""
+	page  = requests.get(url)
+	data = page.text
+	soup = BeautifulSoup(data)
+	return soup
