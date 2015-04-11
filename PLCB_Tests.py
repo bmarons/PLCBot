4ba1b60769459573926c7096a2875c39634ebd9a
Barry <bma30@pitt.edu>
2015-04-10 20:30:04 -0400

    Wrote Failing Test

--- PLCB_Tests.py
+++ PLCB_Tests.py
@@ -2,10 +2,17 @@ import unittest
 import PLCBot
 
 class TestUM(unittest.TestCase):
+
+
 	def test_get_url(self):
 		"""This Test sends a URL to a function and should get back a soup object"""
 		testSoup = PLCBot.get_url("http://example.com/")
 		self.assertEqual(str(testSoup.html.head.title),"<title>Example Domain</title>")
+
+	def test_get_url(self):
+		"""This tests the function that returns the number of items on the search"""
+		self.fail("Not Written")
+
 
 if __name__ == '__main__':
     unittest.main()
