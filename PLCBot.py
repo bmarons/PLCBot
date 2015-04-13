fa6bef685600eefa50d47cebcb5b795466702559
Barry <bma30@pitt.edu>
2015-04-13 19:50:08 -0400

    Moved some things around to fix imports

--- PLCBot.py
+++ PLCBot.py
@@ -43,38 +43,11 @@ def parse_item(item_dec):
 			text = 'NULL'
 		product.append(text)
 	return product
+
 def make_csv(list):
 	"""makes list into CSV"""
 	return ",".join(list) # some times i forget how simple python is
 
-
-f = open(sys.argv[1],'w')
-url = "http://www.lcbapps.lcb.state.pa.us/webapp/product_management/psi_ProductListPage_Inter.asp?strPageNum="+"1"+"&selTyp=Spirits&selTypS=&selTypW=&selTypA=&searchCode=&searchPhrase=&CostRange=&selSale=&strFilter=&prevSortby=BrndNme&sortBy=BrndNme&sortDir=ASC"
-soup = get_url(url)
-num_items = num_items(soup)
-items_retrived = 0
-
-if (num_items%25 ==0):
-	pages = num_items/25
-else:
-	pages = (num_items/25)+1
-
-for j in xrange(1,pages+1):
-
-	url = "http://www.lcbapps.lcb.state.pa.us/webapp/product_management/psi_ProductListPage_Inter.asp?strPageNum="+str(j)+"&selTyp=Spirits&selTypS=&selTypW=&selTypA=&searchCode=&searchPhrase=&CostRange=&selSale=&strFilter=&prevSortby=BrndNme&sortBy=BrndNme&sortDir=ASC"
-	soup = get_url(url)
-
-	if ((num_items - items_retrived) > 25):
-		ids = table_ids(25)
-	else:
-		ids = table_ids(num_items - items_retrived)
-	rows = soup.findAll('tr')
-	for count in xrange(0,len(ids)):
-		product = parse_item(str(rows[ids[count]])+str(rows[ids[int(count)]+2]))
-		clean_string = make_csv(product)
-		print clean_string
-		f.write(clean_string+"\n")
-	items_retrived = items_retrived + 25
 
 
 
---
+++ SaveBot.py
@@ -0,0 +1,32 @@
+import PLCBot
+
+f = open("Output.txt",'w')
+url = "http://www.lcbapps.lcb.state.pa.us/webapp/product_management/psi_ProductListPage_Inter.asp?strPageNum="+"1"+"&selTyp=Spirits&selTypS=&selTypW=&selTypA=&searchCode=&searchPhrase=&CostRange=&selSale=&strFilter=&prevSortby=BrndNme&sortBy=BrndNme&sortDir=ASC"
+soup = PLCBot.get_url(url)
+num_items = PLCBot.num_items(soup)
+items_retrived = 0
+
+if (num_items%25 ==0):
+	pages = num_items/25
+else:
+	pages = (num_items/25)+1
+
+for j in xrange(1,pages+1):
+
+	url = "http://www.lcbapps.lcb.state.pa.us/webapp/product_management/psi_ProductListPage_Inter.asp?strPageNum="+str(j)+"&selTyp=Spirits&selTypS=&selTypW=&selTypA=&searchCode=&searchPhrase=&CostRange=&selSale=&strFilter=&prevSortby=BrndNme&sortBy=BrndNme&sortDir=ASC"
+	soup = PLCBot.get_url(url)
+
+	if ((num_items - items_retrived) > 25):
+		ids = PLCBot.table_ids(25)
+	else:
+		ids = PLCBot.table_ids(num_items - items_retrived)
+	rows = soup.findAll('tr')
+	for count in xrange(0,len(ids)):
+		product = PLCBot.parse_item(str(rows[ids[count]])+str(rows[ids[int(count)]+2]))
+		clean_string = PLCBot.make_csv(product)
+		print clean_string
+		f.write(clean_string+"\n")
+	items_retrived = items_retrived + 25
+
+
+
